a4c21e436949522961f8c71bb8539efdb294d1b3
MantuXVR81 <39915756+MantuXVR81@users.noreply.github.com>
2018-06-16 18:43:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
-print("Seleccione su medida")
-print("1. metros")
-print("2. kilometros")
-print("3. centimetros")
-medida = int(input("Escriba el número de la medida que ha seleccionado:"))
-if medida == 1:
-  medida2 = "metros"
-elif medida == 2:
-  medida2 = "kilometros"
-elif medida == 3:
-  medida2 = "centimetros"
-valor = float(input("Escriba la cantidad que desee: "))
-print("Ha seleccionado", medida2)
-if medida == 1:
-  valor = valor * 1
-elif medida == 2:
-  valor = valor * 1000
-elif medida == 3:
-  valor = valor * 0.01
-print("Seleccione su medida")
-print("1. metros")
-print("2. kilometros")
-print("3. centimetros")
-medida = int(input("Escriba el número de la medida que ha seleccionado: "))
-if medida == 1:
-  valor = valor * 1
-  medida2 = "m"
-elif medida == 2:
-  valor = valor * 0.001
-  medida2 = "km"
-elif medida == 3:
-  valor = valor *100
-  medida2 = "cm"
-
-print("El valor es: ", valor, medida2)
+import math
+S=["Sexagesimal","sexagesimal","S"]
+C=["Centesimal","centesimal","C"]
+R=["Radianes","radianes","R"]
+def conversor7(n,m):
+  n=input("Ingrese valor numérico: ")
+  m=input("Ingrese medida del ángulo: ")
+  if m in S:
+    k=10*n/9
+    s= print(k,"C")
+    return s
+def conversor8(n,m):
+  n=input("Ingrese valor numérico: ")
+  m=input("Ingrese medida del ángulo: ")
+  if m in C:
+    k=9*n/10
+    s= print(k,"S")
+    return s
+def conversor9(n,m):
+  n=input("Ingrese valor numérico: ")
+  m=input("Ingrese medida del ángulo: ")
+  if m in R:
+    k=180*n/math.pi
+    s= print(k,"S")
+    return s
+def conversor10(n,m):
+  n=input("Ingrese valor numérico: ")
+  m=input("Ingrese medida del ángulo: ")
+  if m in R:
+    k=200*n/math.pi
+    s= print(k,"C")
+    return s
+def conversor11(n,m):
+  n=input("Ingrese valor numérico: ")
+  m=input("Ingrese medida del ángulo: ")
+  if m in S:
+    k=math.pi*n/180
+    s= print(k,"R")
+    return s
+def conversor7(n,m):
+  n=input("Ingrese valor numérico: ")
+  m=input("Ingrese medida del ángulo: ")
+  if m in C:
+    k=math.pi*n/200
+    s= print(k,"R")
+    return s
